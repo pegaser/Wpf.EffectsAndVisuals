2d7240cdef17b43bc5a8e4d389e3e8436d3ea1ec
pegaser <sperezgabriel@gmail.com>
2023-05-19 16:17:07 -0600

    Agregar archivos de proyecto.

---
+++ Wpf.EffectsAndVisuals/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf.EffectsAndVisuals
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            DrawingVisual visual = new DrawingVisual();
+            using(DrawingContext dc = visual.RenderOpen())
+            {
+                dc.DrawRectangle(Brushes.Red, new Pen(Brushes.Black, 2), new Rect(0, 0, 100, 100));
+            }
+        }
+    }
+}
